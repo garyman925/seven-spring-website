cfa2e6d500263525f58bd7221395cb4210bb4db9
garyman925 <garyman925@mail.com>
2024-11-28 14:50:01 +0800

    Finish the new 11+ pagee

--- 11-entrance-exams.php
+++ 11-entrance-exams.php
@@ -4,52 +4,78 @@
 <section class="section">
 <div class="container">
 
-<img src="images/top-banner/11-entrance-exams.png" width="100%" alt="Heading: 11+ Entrance Exams">
+<img src="images/11-entrance-exams/header-banner.png" width="100%" alt="Heading: 11+ Entrance Exams">
 
-<h1>Short overview</h1>
-<p>At Seven Springs Education, we offer a comprehensive range of programmes to help your child prepare for the 11+ entrance exams, whether they're aiming for grammar or independent schools. From personalised tutoring to intensive study skills guidance and workshops, our courses provide targeted support in key subjects, including Verbal Reasoning, Non-Verbal Reasoning, Maths, and English. We understand the distinct requirements of both grammar and independent school exams, ensuring our programmes equip your child with the skills and confidence they need to excel on their education journey.</p>
+<div class="mb-5">
+    <h2 class="strong text-primary mb-3">Short overview</h2>
+    <p>At Seven Springs Education, we offer a comprehensive range of programmes to help your child prepare for the 11+ entrance exams, whether they're aiming for grammar or independent schools. From personalised tutoring to intensive study skills guidance and workshops, our courses provide targeted support in key subjects, including <strong>Verbal Reasoning</strong>, <strong>Non-Verbal Reasoning</strong>, <strong>Maths</strong>, and English. We understand the distinct requirements of both grammar and independent school exams, ensuring our programmes equip your child with the skills and confidence they need to excel on their education journey.</p>
+</div>
 
-<div>
-    <h1>11+ Preparation Courses</h1>
+<hr>
+
+<div class="mb-5">
+    <h2 class="strong text-primary mb-3">11+ Preparation Courses</h2>
     <p>Whether your child is gearing up for grammar school exams, independent school entry, or looking to excel in core subjects, we have the perfect course. With free trials, holiday intensives, and ongoing courses, we provide the flexible learning solutions your child needs for success.</p>
-    <button class="btn btn-primary">11+ Courses</button>
+    <button class="btn btn-lg btn-primary">11+ Courses</button>
 </div>
  
+<hr>
 
-<h1>Our Tutors</h1>
-<div>
-    <h3>Our 11+ Expert Tutors</h3>
-    <p>Our 11+ expert tutors are carefully handpicked from the UK's most prestigious universities, specialising in the subjects they teach. Among them are our two lead specialists, Willow and Ceri, who bring a wealth of knowledge and years of experience in guiding students through the challenging admissions processes for grammar and independent schools. Alongside them, our dedicated team of skilled tutors is committed to helping every child reach their full potential, providing tailored support and expert guidance. Together, they create an environment where each student can thrive and excel in their 11+ preparation.</p>
-    <img src="images/11-entrance-exams/tutors.png" width="100%" alt="Our 11+ Expert Tutors">
-    <img src="images/11-entrance-exams/tutors.png" width="100%" alt="Our 11+ Expert Tutors">
+<h1 class="strong text-primary mb-3">Our Tutors</h1>
+
+<div class="mb-5 tutor-section">
+    <h3 class="strong text-primary mb-3">Our 11+ Expert Tutors</h3>
+    <p>Our 11+ expert tutors are carefully handpicked from the UK's most prestigious universities, specialising in the subjects they teach. Among them are our <strong>two lead specialists</strong>, <strong>Willow</strong> and <strong>Ceri</strong>, who bring a wealth of knowledge and years of experience in guiding students through the challenging admissions processes for grammar and independent schools. Alongside them, our dedicated team of skilled tutors is committed to helping every child reach their full potential, providing tailored support and expert guidance. Together, they create an environment where each student can thrive and excel in their 11+ preparation.</p>
+    <div class="row">   
+        <div class="col-12 col-md-6 text-center">
+            <div class="tutor-image-container">
+                <img src="images/tutor/willow-hewitt.jpg" alt="Our 11+ Expert Tutors">
+                <div class="tutor-name">Willow Hewitt</div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 text-center">
+            <div class="tutor-image-container">
+                <img src="images/tutor/ceri-moss.png" alt="Our 11+ Expert Tutors">
+                <div class="tutor-name">Ceri Moss</div>
+            </div>
+        </div>
+    </div>
 </div>
 
+<hr>
 
-<h1>Free Resources</h1>
+<h1 class="strong text-primary mb-3">Free Resources</h1>
 
-<div class="row">
+<div class="row mb-5">
     <div class="col-12 col-md-6">
-        <img src="images/11-entrance-exams/free-resources.png" width="100%" alt="Free Resources">
+        <img src="images/11-entrance-exams/free-resources.jpg" width="100%" alt="Free Resources">
     </div>
     <div class="col-12 col-md-6">
-        <h3>Free Resources</h3>
+        <h3 class="strong text-primary mb-3">Free Resources</h3>
         <p>Explore our collection of free 11+ resources designed to support your child's exam preparation. From familiarisation tests to helpful guides, we've got everything you need to give them an extra edge. Click the button below to access and start using these valuable tools today!</p>
-        <button class="btn btn-primary">Free Resources</button>
+        <a href="https://www.seven-springs.co.uk/resource-hub/11-resources/#">
+            <button class="btn btn-lg btn-primary">Free Resources</button>
+        </a>
     </div>
 </div>
 
-<h1>Contact Us</h1>
+<hr>
 
-<div class="row">
+<h1 class="strong text-primary mb-3">Contact Us</h1>
+
+<div class="row mb-5">
     <div class="col-12 col-md-6">
-        <img src="images/11-entrance-exams/contact-us.png" width="100%" alt="Contact Us">
+        <img src="images/11-entrance-exams/contacts.jpg" width="100%" alt="Contact Us">
     </div>
     <div class="col-12 col-md-6">
-        <h3>In need of guidance?</h3>
+        <h3 class="strong text-primary mb-3">In need of guidance?</h3>
         <p>We're dedicated to supporting your child's journey towards success. If you'd like to create a tailored learning plan or
         need information about the specific topics on the 11+
         exam, don't hesitate to reach out! We're here to help every step of the way.</p>
-        <button class="btn btn-primary">Contact Us</button>
+        
+        <a href="https://www.seven-springs.co.uk/contact-us/">
+            <button class="btn btn-lg btn-primary">Contact Us</button>
+        </a>
     </div>
 </div>
 
